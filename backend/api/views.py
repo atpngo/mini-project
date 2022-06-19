a14ddfd5aa57e2186be3cfba4daccb3b03e7d359
Andy Ngo <andyngo2021@gmail.com>
2022-06-18 22:09:59 -0700

    successful pagination for function based views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Item, Threshold, Powerline
 from .serializers import ItemSerializer, ThresholdSerializer, PowerlineSerializer
+from rest_framework.pagination import PageNumberPagination
 
 @api_view(['GET'])
 def getData(request):
@@ -32,9 +33,12 @@ def editThreshold(request):
 
 @api_view(['GET'])
 def getPowerlines(request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 2
     items = Powerline.objects.all()
-    serializer = PowerlineSerializer(items, many=True)
-    return Response(serializer.data)
+    result = paginator.paginate_queryset(items, request)
+    serializer = PowerlineSerializer(result, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 @api_view(['POST'])
 def addPowerline(request):
