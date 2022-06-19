2b61f2b3012ef67c5165f66d674e039a06eb23c5
Andy Ngo <andyngo2021@gmail.com>
2022-06-18 23:26:55 -0700

    plotted a powerline successfully

---
+++ format_line.py
@@ -0,0 +1,24 @@
+import pandas
+import pprint
+
+data = pandas.read_csv('challenge_source_data.csv')
+data_len = len(data)
+
+line = eval(data.loc[0, 'geometry'])
+coords = line['coordinates']
+# pprint.pprint(coords)
+
+final = []
+for shape in coords:
+    s = []
+    for coord in shape:
+        s.append(coord[::-1])
+    final.append(s)
+
+pprint.pprint(final)
+seen = set()
+output_string = ''
+import pyperclip; pyperclip.copy(pprint.pformat(str(coords)))
+
+print('-'*100)
+# print(line['bbox'])
