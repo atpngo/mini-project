5dfca60ebf31c4a0570a54c5a7042453d2ff3b34
Andy Ngo <andyngo2021@gmail.com>
2022-06-18 19:33:51 -0700

    working script and sent all data to backend

--- upload.py
+++ upload.py
@@ -1,21 +1,28 @@
 # This python scripts reads in from a csv and makes a post request to add it to our backend
 import requests
 import json
+import pandas
 
-data = dict()
+data = pandas.read_csv('challenge_source_data.csv')
+data_len = len(data)
 
-data['geometry'] = {'bbox': [-121.706038329478, 37.700539173403726, -121.70587308768884, 37.70058992865414], 'type': 'MultiLineString', 'coordinates': [[[-121.70587308768884, 37.70058992865414], [-121.70591438209864, 37.70058130395718], [-121.70597654029899, 37.700560605487354]], [[-121.70597654029899, 37.700560605487354], [-121.7060100332222, 37.70054945234938]], [[-121.7060100332222, 37.70054945234938], [-121.706038329478, 37.700539173403726]]]}
-data['wear'] = 0.1
-data['weather'] = 0.2
-data['vegetation'] = 0.3
-data['name'] = 'Test Name'
+def send_data(payload, i, data_len):
+    r = requests.post('http://localhost:8000/add-powerline/', data=json.dumps(payload), headers={'content-type':'application/json'})
+    if r.status_code != 200:
+        exit(1)
+    else:
+        print(f'Sent powerline ({i+1}/{data_len}) - {round(((i+1)/data_len)*100, 1)}%')
+    return
 
-# print(data)
+for i in range(data_len):
+    payload = dict()
+    payload['geometry'] = eval(data.loc[i, 'geometry'])
+    payload['wear'] = data.loc[i, 'wear']
+    payload['weather'] = data.loc[i, 'weather']
+    payload['vegetation'] = data.loc[i, 'vegetation']
+    payload['name'] = data.loc[i, 'names']
+    send_data(payload, i, data_len)
 
-
-r = requests.post('http://localhost:8000/add-powerline/', data=json.dumps(data), headers={'content-type':'application/json'})
-print(r)
-print(r.text)
 
 # r = requests.get('http://localhost:8000/powerlines/')
 # print(r)
