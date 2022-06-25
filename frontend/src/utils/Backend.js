ac091d320e0a9f8b536b91db0482d0b7b89265ca
Andy Ngo <andyngo2021@gmail.com>
2022-06-25 06:19:01 -0700

    fixed

--- frontend/src/utils/Backend.js
+++ frontend/src/utils/Backend.js
@@ -1,15 +1,25 @@
 class Backend
 {
-    static getLineURL = 'http://localhost:8000/powerlines/';
-    static getLineByPageURL = 'http://localhost:8000/powerlines/?page=';
-    static updateLineURL = 'http://localhost:8000/update-powerline/';
-    static deleteLineURL = 'http://localhost:8000/delete-powerline/';
-    static createLineURL = 'http://localhost:8000/add-powerline/';
-    static thresholdURL = 'http://localhost:8000/edit-threshold/';
-    static registerURL = 'http://localhost:8000/api/register/';
-    static getTokenURL = 'http://localhost:8000/api/token/';
-    static refreshTokenURL = 'http://localhost:8000/api/token/refresh/';
-
-
-    
+    // deployment
+    static getLineURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/powerlines/';
+    static getLineByPageURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/powerlines/?page=';
+    static updateLineURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/update-powerline/';
+    static deleteLineURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/delete-powerline/';
+    static createLineURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/add-powerline/';
+    static thresholdURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/edit-threshold/';
+    static registerURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/api/register/';
+    static getTokenURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/api/token/';
+    static refreshTokenURL = 'https://snivelinganxiousdestination.andyngo6.repl.co/api/token/refresh/';   
+    // local dev
+    // static getLineURL = 'http://localhost:8000/powerlines/';
+    // static getLineByPageURL = 'http://localhost:8000/powerlines/?page=';
+    // static updateLineURL = 'http://localhost:8000/update-powerline/';
+    // static deleteLineURL = 'http://localhost:8000/delete-powerline/';
+    // static createLineURL = 'http://localhost:8000/add-powerline/';
+    // static thresholdURL = 'http://localhost:8000/edit-threshold/';
+    // static registerURL = 'http://localhost:8000/api/register/';
+    // static getTokenURL = 'http://localhost:8000/api/token/';
+    // static refreshTokenURL = 'http://localhost:8000/api/token/refresh/';   
 }
+
+export default Backend;
